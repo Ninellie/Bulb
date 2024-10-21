39bdd57c43b094a557f987ba5073cb97342aa76f
Ninellie <a.pawlow2009@ya.ru>
2024-10-21 03:57:18 +0300

    Added EyeController.cs for cute eye animation

---
+++ Assets/_project/Scripts/EntityComponents/EyeController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace EntityComponents
+{
+    public class EyeController : MonoBehaviour
+    {
+        [SerializeField] private Transform core;
+        
+        // Максимум сдвига от центра
+        [SerializeField] private float deviationMax;
+        
+        // Скорость реакции глаза
+        [SerializeField] private float deviationPower;
+
+        private Transform _eye;
+        
+        private Vector2 _previousPosition;
+        
+        private void Awake()
+        {
+            _eye = transform;
+        }
+
+        private void Start()
+        {
+            _previousPosition = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            // Текущая позиция записана
+            var currentPosition = (Vector2)core.position;
+            
+            // Разница между позициями между двумя fixed кадрами, это расстояние пройдённое между кадрами, оно же - скорость объекта
+            var posDif = currentPosition - _previousPosition;
+            
+            posDif.Normalize();
+            
+            posDif *= deviationPower;
+            
+            var nextLocalPos = (Vector2)_eye.localPosition + posDif;
+
+            var finalPosition = Vector2.ClampMagnitude(nextLocalPos, deviationMax);
+            
+            _eye.localPosition = finalPosition;
+            
+            _previousPosition = currentPosition;
+        }
+    }
+}
