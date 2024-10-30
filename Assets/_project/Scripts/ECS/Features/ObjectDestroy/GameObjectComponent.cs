fc1d7b379ef6effc48f10f7698b61480e1c0f2c8
Ninellie <a.pawlow2009@ya.ru>
2024-10-31 02:32:50 +0300

    ObjectDestroy added

---
+++ Assets/_project/Scripts/ECS/Features/ObjectDestroy/GameObjectComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Scellecs.Morpeh;
+using UnityEngine;
+
+namespace _project.Scripts.ECS.Features.ObjectDestroyer
+{
+    [Serializable]
+    public struct GameObjectComponent : IComponent
+    {
+        [SerializeField] private GameObject gameObject;
+
+        public GameObject GameObject
+        {
+            get => gameObject;
+            set => gameObject = value;
+        }
+    }
+}
---
+++ Assets/_project/Scripts/ECS/Features/ObjectDestroy/GameObjectProvider.cs
@@ -0,0 +1,9 @@
+﻿using Scellecs.Morpeh.Providers;
+using Unity.IL2CPP.CompilerServices;
+
+namespace _project.Scripts.ECS.Features.ObjectDestroyer
+{
+    public sealed class GameObjectProvider : MonoProvider<GameObjectComponent>
+    {
+    }
+}
