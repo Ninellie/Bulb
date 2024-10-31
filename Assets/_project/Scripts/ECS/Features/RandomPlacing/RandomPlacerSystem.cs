51330355f00351d91511a8150a0d379a4aa2ec33
Ninellie <a.pawlow2009@ya.ru>
2024-10-31 03:23:01 +0300

    Random placer system fix

--- Assets/_project/Scripts/ECS/Features/RandomPlacing/RandomPlacerSystem.cs
+++ Assets/_project/Scripts/ECS/Features/RandomPlacing/RandomPlacerSystem.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using _project.Scripts.ECS.Features.Spawner;
 using Core.Shapes;
 using Scellecs.Morpeh;
 using Scellecs.Morpeh.Systems;
 using UnityEngine;
+using PixelPerfectCamera = UnityEngine.Experimental.Rendering.Universal.PixelPerfectCamera;
 
 namespace _project.Scripts.ECS.Features.RandomPlacing
 {
@@ -12,7 +12,7 @@ namespace _project.Scripts.ECS.Features.RandomPlacing
     public sealed class RandomPlacerSystem : FixedUpdateSystem
     {
         [SerializeField] private CheckMode placeAreaCheckMode;
-        
+
         private readonly List<IShape> _placeBlockAreas = new();
         private readonly List<IShape> _placeAreas = new();
 
@@ -45,16 +45,15 @@ namespace _project.Scripts.ECS.Features.RandomPlacing
         private void InitSpawnAreas()
         {
             var cam = Camera.main;
-
             if (cam == null) throw new NullReferenceException("Main camera is null");
-            
-            var height = cam.scaledPixelHeight;
-            var width = cam.scaledPixelWidth;
 
-            var halfWidth =  width / 2;
+            var pixelCam = cam.GetComponent<PixelPerfectCamera>();
+            var height = pixelCam.refResolutionY;
+            var width = pixelCam.refResolutionX;
+
             var halfHeight =  height/ 2;
-            var xMin = width - halfWidth;
-            var xMax = width + halfWidth;
+            var xMin = 0 - width;
+            var xMax = 0 + width;
             var yMin = height + halfHeight;
             var yMax = height + height;
             
