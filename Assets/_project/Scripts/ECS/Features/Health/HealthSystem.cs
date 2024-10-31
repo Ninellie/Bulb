0261d14f5b4114b690732a4e544f22250f191534
Ninellie <a.pawlow2009@ya.ru>
2024-10-31 03:46:29 +0300

    Fixed Health System

--- Assets/_project/Scripts/ECS/Features/Health/HealthSystem.cs
+++ Assets/_project/Scripts/ECS/Features/Health/HealthSystem.cs
@@ -1,4 +1,5 @@
 using _project.Scripts.ECS.Features.ObjectDestroy;
+using _project.Scripts.ECS.Features.Spawner;
 using Scellecs.Morpeh;
 using Scellecs.Morpeh.Systems;
 using UnityEngine;
@@ -26,7 +27,7 @@ namespace _project.Scripts.ECS.Features.Health
                 {
                     continue;
                 }
-                entity.AddComponent<DestroyRequest>();
+                entity.AddComponent<ReleaseRequest>();
             }
         }
     }
