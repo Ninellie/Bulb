db9a9b24ce88af75e7e8fff44a4ceb355eceecba
Ninellie <a.pawlow2009@ya.ru>
2024-11-01 01:14:23 +0300

    Fixed Health system

--- Assets/_project/Scripts/ECS/Features/Health/HealthSystem.cs
+++ Assets/_project/Scripts/ECS/Features/Health/HealthSystem.cs
@@ -1,13 +1,16 @@
-using _project.Scripts.ECS.Features.ObjectDestroy;
 using _project.Scripts.ECS.Features.Spawner;
 using Scellecs.Morpeh;
 using Scellecs.Morpeh.Systems;
+using Unity.IL2CPP.CompilerServices;
 using UnityEngine;
 
 namespace _project.Scripts.ECS.Features.Health
 {
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
     [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(HealthSystem))]
-    public sealed class HealthSystem : FixedUpdateSystem
+    public sealed class HealthSystem : FixedUpdateSystem 
     {
         private Filter _filter;
         private Stash<Health> _healthStash;
