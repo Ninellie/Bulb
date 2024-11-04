84b417b255aa734be115b441acaf1d62441dff97
Ninellie <a.pawlow2009@ya.ru>
2024-11-04 04:28:12 +0300

    Health system refactoring

--- Assets/_project/Scripts/ECS/Features/Health/HealthSystem.cs
+++ Assets/_project/Scripts/ECS/Features/Health/HealthSystem.cs
@@ -12,14 +12,12 @@ namespace _project.Scripts.ECS.Features.Health
     [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(HealthSystem))]
     public sealed class HealthSystem : FixedUpdateSystem 
     {
-        private Filter _healthFilter;
         private Filter _healthDecreaseRequestFilter;
         private Stash<HealthComponent> _healthStash;
         private Stash<HealthChangeRequest> _healthDecreaseRequestStash;
     
         public override void OnAwake()
         {
-            _healthFilter = World.Filter.With<HealthComponent>().Build();
             _healthStash = World.GetStash<HealthComponent>();
             
             _healthDecreaseRequestFilter = World.Filter.With<HealthChangeRequest>().Build();
@@ -35,17 +33,6 @@ namespace _project.Scripts.ECS.Features.Health
                 ref var health = ref _healthStash.Get(request.TargetEntity);
                 health.HealthPoints += request.Amount;
                 entity.RemoveComponent<HealthChangeRequest>();
-            }
-            
-            // Находит все сущности с нулевым здоровьем и создаёт запрос на возвращение в пул
-            foreach (var entity in _healthFilter)
-            {
-                ref var healthComponent = ref _healthStash.Get(entity);
-                if (healthComponent.HealthPoints > 0)
-                {
-                    continue;
-                }
-                entity.AddComponent<ReleaseRequest>();
             }
         }
     }
