2fe296f07d22a42ccb91e3dcd70d2802d8f7702e
Ninellie <a.pawlow2009@ya.ru>
2024-11-07 01:30:44 +0300

    Added TilemapVariable.cs class

---
+++ Assets/_project/Scripts/Core/Variables/TilemapVariable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Core.Variables
+{
+    [CreateAssetMenu(fileName = "New Tilemap Variable", menuName = "Variables/Tilemap", order = 51)]
+    public class TilemapVariable : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string developerDescription = "";
+#endif
+        public Tilemap value;
+
+        public void SetValue(Tilemap value)
+        {
+            this.value = value;
+        }
+
+        public void SetValue(TilemapVariable value)
+        {
+            this.value = value.value;
+        }
+    }
+}
