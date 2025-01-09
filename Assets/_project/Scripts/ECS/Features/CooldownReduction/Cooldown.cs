f4b88752e07f8ee34d15b5139efd220f77687376
Ninellie <a.pawlow2009@ya.ru>
2025-01-09 05:32:03 +0300

    Added CooldownReductionSystem.cs system

---
+++ Assets/_project/Scripts/ECS/Features/CooldownReduction/Cooldown.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Scellecs.Morpeh;
+using UnityEngine;
+
+namespace _project.Scripts.ECS.Features.CooldownReduction
+{
+    [Serializable]
+    public struct Cooldown : IComponent
+    {
+        [field: SerializeField] public float Current { get; set; }
+    }
+}
---
+++ Assets/_project/Scripts/ECS/Features/CooldownReduction/CooldownReductionSystem.cs
@@ -0,0 +1,37 @@
+﻿using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using Unity.IL2CPP.CompilerServices;
+using UnityEngine;
+
+namespace _project.Scripts.ECS.Features.CooldownReduction
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(CooldownReductionSystem))]
+    public sealed class CooldownReductionSystem : FixedUpdateSystem
+    {
+        private Filter _nonFrozenCooldownFilter;
+        private Stash<Cooldown> _cooldownStash;
+        
+        public override void OnAwake()
+        {
+            _nonFrozenCooldownFilter = World.Filter
+                .With<Cooldown>()
+                //.Without<Frozen>()
+                .Build();
+            _cooldownStash = World.GetStash<Cooldown>();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            foreach (var entity in _nonFrozenCooldownFilter)
+            {
+                ref var cooldown = ref _cooldownStash.Get(entity);
+                cooldown.Current -= deltaTime;
+                if (cooldown.Current > 0) continue;
+                _cooldownStash.Remove(entity);
+            }
+        }
+    }
+}
