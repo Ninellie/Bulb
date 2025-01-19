8e286cbcf856aa44d7af06255bf039e41aa6374f
Ninellie <a.pawlow2009@ya.ru>
2025-01-19 17:10:35 +0300

    Added EnergyGlowSystem code

---
+++ Assets/_project/Scripts/ECS/Features/Glowing/EnergyGlowSystem.cs
@@ -0,0 +1,35 @@
+﻿using _project.Scripts.ECS.Features.EnergyConsumption;
+using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using Unity.IL2CPP.CompilerServices;
+using UnityEngine;
+
+namespace _project.Scripts.ECS.Features.Glowing
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [CreateAssetMenu(menuName = "ECS/Systems/Update/" + nameof(EnergyGlowSystem))]
+    public sealed class EnergyGlowSystem : UpdateSystem
+    {
+        private Filter _glowingEnergyContainersFilter;
+        
+        public override void OnAwake()
+        {
+            _glowingEnergyContainersFilter = World.Filter
+                .With<Glowing>()
+                .With<EnergyReserve>()
+                .Build();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            foreach (var entity in _glowingEnergyContainersFilter)
+            {
+                ref var glowing = ref entity.GetComponent<Glowing>();
+                ref var energyReserve = ref entity.GetComponent<EnergyReserve>();
+                glowing.Light2D.pointLightOuterRadius = energyReserve.CurrentAmount * 35;
+            }
+        }
+    }
+}
---
+++ Assets/_project/Scripts/ECS/Features/Glowing/Glowing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Scellecs.Morpeh;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace _project.Scripts.ECS.Features.Glowing
+{
+    [Serializable]
+    public struct Glowing : IComponent
+    {
+        [field: SerializeField] public Light2D Light2D { get; set; } 
+    }
+}
