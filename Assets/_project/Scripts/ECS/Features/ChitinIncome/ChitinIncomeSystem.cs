c11694d89b8651e5ed42187dfaff0db6438ed26b
Ninellie <a.pawlow2009@ya.ru>
2025-02-03 23:43:18 +0300

    Added ChitinIncomeSystem.cs

---
+++ Assets/_project/Scripts/ECS/Features/ChitinIncome/ChitinIncomeSystem.cs
@@ -0,0 +1,19 @@
+﻿using Scellecs.Morpeh.Systems;
+using UnityEngine;
+
+namespace _project.Scripts.ECS.Features.ChitinIncome
+{
+    [CreateAssetMenu(menuName = "ECS/Systems/Fixed/" + nameof(ChitinIncomeSystem))]
+    public sealed class ChitinIncomeSystem : FixedUpdateSystem
+    {
+        public override void OnAwake()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
