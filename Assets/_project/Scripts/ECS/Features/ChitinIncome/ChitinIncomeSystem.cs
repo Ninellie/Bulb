c1530f47d8e41caede9f6f5d74c264f155b1c3b9
Ninellie <a.pawlow2009@ya.ru>
2025-02-04 00:12:14 +0300

    Added logic in ChitinIncomeSystem.cs

--- Assets/_project/Scripts/ECS/Features/ChitinIncome/ChitinIncomeSystem.cs
+++ Assets/_project/Scripts/ECS/Features/ChitinIncome/ChitinIncomeSystem.cs
@@ -1,4 +1,7 @@
-﻿using Scellecs.Morpeh.Systems;
+﻿using _project.Scripts.Core.Variables;
+using _project.Scripts.ECS.Features.Spawner;
+using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
 using UnityEngine;
 
 namespace _project.Scripts.ECS.Features.ChitinIncome
@@ -6,14 +9,21 @@ namespace _project.Scripts.ECS.Features.ChitinIncome
     [CreateAssetMenu(menuName = "ECS/Systems/Fixed/" + nameof(ChitinIncomeSystem))]
     public sealed class ChitinIncomeSystem : FixedUpdateSystem
     {
+        [SerializeField] private FloatVariable currentCurrency;
+        
+        private Stash<EnemyReturnToPoolEvent> _returnToPoolEventStash;
+        
         public override void OnAwake()
         {
-            throw new System.NotImplementedException();
+            _returnToPoolEventStash = World.GetStash<EnemyReturnToPoolEvent>();
         }
 
         public override void OnUpdate(float deltaTime)
         {
-            throw new System.NotImplementedException();
+            foreach (var returnEvent in _returnToPoolEventStash)
+            {
+                currentCurrency.value += 1;
+            }
         }
     }
 }
