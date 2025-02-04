c450185cf550de061e9d350ef4dc1dd4ae21a0e8
Ninellie <a.pawlow2009@ya.ru>
2025-02-05 02:46:47 +0300

    Revert "Deleted Visability System"
    This reverts commit dfdd8c5974d470be6f39b6f8af86031222569188.

---
+++ Assets/_project/Scripts/ECS/Features/Visability/Rendered.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Scellecs.Morpeh;
+using UnityEngine;
+
+namespace _project.Scripts.ECS.Features.Visability
+{
+    [Serializable]
+    public struct Rendered : IComponent
+    {
+        [field:SerializeField] public Renderer Renderer { get; set; }
+    }
+}
---
+++ Assets/_project/Scripts/ECS/Features/Visability/VisabilitySystem.cs
@@ -0,0 +1,65 @@
+﻿using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using Unity.IL2CPP.CompilerServices;
+using UnityEngine;
+
+namespace _project.Scripts.ECS.Features.Visability
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [CreateAssetMenu(menuName = "ECS/Systems/Update/" + nameof(VisabilitySystem))]
+    public sealed class VisabilitySystem : UpdateSystem
+    {
+        private Filter _renderedFilter;
+        private Stash<Rendered> _rendered;
+        private Stash<Visible> _visible;
+        private Stash<Invisible> _invisible;
+
+        public override void OnAwake()
+        {
+            _renderedFilter = World.Filter.With<Rendered>().Build();
+            _rendered = World.GetStash<Rendered>();
+            _visible = World.GetStash<Visible>();
+            _invisible = World.GetStash<Invisible>();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            foreach (var entity in _renderedFilter)
+            {
+                if (!_rendered.Has(entity))
+                {
+                    continue;
+                }
+                
+                ref var rendered = ref entity.GetComponent<Rendered>();
+
+                var isVisible = rendered.Renderer.isVisible;
+
+                if (isVisible)
+                {
+                    if (_invisible.Has(entity))
+                    {
+                        entity.RemoveComponent<Invisible>();
+                    }
+                    if (!_visible.Has(entity))
+                    {
+                        entity.AddComponent<Visible>();
+                    }
+                }
+                else
+                {
+                    if (_visible.Has(entity))
+                    {
+                        entity.RemoveComponent<Visible>();
+                    }
+                    if (!_invisible.Has(entity))
+                    {
+                        entity.AddComponent<Invisible>();
+                    }
+                }
+            }
+        }
+    }
+}
