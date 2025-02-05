c664b0f96c929784d70110dd1b131cf8e9481057
Ninellie <a.pawlow2009@ya.ru>
2025-02-05 03:04:28 +0300

    Added Game Reset System

---
+++ Assets/_project/Scripts/ECS/Features/GameReset/GameResetSystem.cs
@@ -0,0 +1,28 @@
+﻿using _project.Scripts.ECS.Features.GameCoreEntity;
+using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _project.Scripts.ECS.Features.GameReset
+{
+    /// <summary>
+    ///  Отслеживает события смерти ключевых сущностей и перезагружает сцену, если находит.
+    /// </summary>
+    [CreateAssetMenu(menuName = "ECS/Systems/Fixed/" + nameof(GameResetSystem))]
+    public sealed class GameResetSystem : FixedUpdateSystem
+    {
+        private Stash<GameCoreEntityDeathEvent> _deathEventStash;
+        
+        public override void OnAwake()
+        {
+            _deathEventStash = World.GetStash<GameCoreEntityDeathEvent>();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            if (_deathEventStash.Length <= 0) return;
+            SceneManager.LoadScene(0);
+        }
+    }
+}
