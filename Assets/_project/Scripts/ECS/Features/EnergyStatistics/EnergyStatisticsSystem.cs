1a06b83fe64cd58f628c420830fadace2b66b294
Ninellie <a.pawlow2009@ya.ru>
2025-02-13 04:26:58 +0300

    Added Energy Statistics System

---
+++ Assets/_project/Scripts/ECS/Features/EnergyStatistics/EnergyStatisticsSystem.cs
@@ -0,0 +1,65 @@
+﻿using _project.Scripts.Core.Variables;
+using _project.Scripts.ECS.Features.EnergyProduction;
+using _project.Scripts.ECS.Features.EnergyReserving;
+using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using UnityEngine;
+
+namespace _project.Scripts.ECS.Features.EnergyStatistics
+{
+    [CreateAssetMenu(menuName = "ECS/Systems/Fixed/" + nameof(EnergyStatisticsSystem))]
+    public sealed class EnergyStatisticsSystem : FixedUpdateSystem
+    {
+        [SerializeField] private FloatVariable currentEnergy;
+        [SerializeField] private FloatVariable maximumEnergy;
+        [SerializeField] private FloatVariable generationRate;
+
+        private Filter _accumulatorFilter;
+        
+        private Stash<Generator> _generatorsStash;
+        
+        public override void OnAwake()
+        {
+            _accumulatorFilter = World.Filter
+                .With<EnergyContainer>()
+                .With<EnergyOutput>()
+                .With<EnergyInput>()
+                .Build();
+            
+            _generatorsStash = World.GetStash<Generator>();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            var energyCurrent = 0f;
+            var energyMax = 0f;
+            
+            foreach (var entity in _accumulatorFilter)
+            {
+                ref var energyContainer = ref entity.GetComponent<EnergyContainer>();
+                energyCurrent += energyContainer.CurrentAmount;
+                energyMax += energyContainer.MaximumAmount;
+            }
+            
+            currentEnergy.SetValue(energyCurrent);
+            maximumEnergy.SetValue(energyMax);
+
+            var genRate = GetGenerationRate();
+            generationRate.SetValue(genRate);
+        }
+        
+        private float GetGenerationRate()
+        {
+            var generationRatePerSecond = 0f;
+            
+            foreach (ref var generator in _generatorsStash)
+            {
+                var productionAmount = generator.EnergyProductionAmount.Value;
+                var baseCooldown = generator.BaseCooldown.Value;
+                generationRatePerSecond += productionAmount / baseCooldown;
+            }
+
+            return generationRatePerSecond;
+        }
+    }
+}
