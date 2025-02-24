1e2d12eedf048ee59f11ab5fb165e6afc8b58cb1
Ninellie <a.pawlow2009@ya.ru>
2025-02-25 00:29:23 +0300

    Added more info adds to BlockInfoUtils.cs

--- Assets/_project/Scripts/ECS/Features/BlockInfoDisplay/BlockInfoUtils.cs
+++ Assets/_project/Scripts/ECS/Features/BlockInfoDisplay/BlockInfoUtils.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using _project.Scripts.ECS.Features.Aiming;
 using _project.Scripts.ECS.Features.Blocks;
+using _project.Scripts.ECS.Features.Damage;
+using _project.Scripts.ECS.Features.EnergyFeature.EnergyProduction;
 using _project.Scripts.ECS.Features.EnergyFeature.EnergyReserving;
 using Scellecs.Morpeh;
 
@@ -15,7 +18,7 @@ namespace _project.Scripts.ECS.Features.BlockInfoDisplay
             stats = GetCost(entity, stats);
             stats = GetGenerationRate(entity, stats);
             stats = GetDamage(entity, stats);
-            stats = GetAttackRate(entity, stats);
+            stats = GetShooterStats(entity, stats);
             stats = GetAimingRadius(entity, stats);
             return stats;
         }
@@ -23,56 +26,67 @@ namespace _project.Scripts.ECS.Features.BlockInfoDisplay
         private static Dictionary<string, string> GetName(Entity entity, Dictionary<string, string> stats)
         {
             ref var name = ref entity.GetComponent<BlockName>(out var exist);
-            
             if (!exist) return stats;
-            
             stats.Add("Name", name.Name);
-            
             return stats;
         }
 
         private static Dictionary<string, string> GetEnergy(Entity entity, Dictionary<string, string> stats)
         {
-            ref var energy = ref entity.GetComponent<EnergyContainer>(out var exist);
-
+            ref var container = ref entity.GetComponent<EnergyContainer>(out var exist);
             if (!exist) return stats;
-
-            var maxEnergy = energy.MaximumAmount;
-            var currentEnergy = energy.CurrentAmount;
-
-            stats.Add("Energy", currentEnergy + " / " + maxEnergy);
-
+            var energy = $"{container.CurrentAmount:F2}/{container.MaximumAmount}";
+            stats.Add("Energy", energy);
             return stats;
         }
 
         private static Dictionary<string, string> GetCost(Entity entity, Dictionary<string, string> stats)
         {
             ref var chitinCost = ref entity.GetComponent<BlockChitinCost>(out var exist);
-            
             if (!exist) return stats;
-            
             stats.Add("Chitin Cost", chitinCost.Cost.ToString());
-            
             return stats;
         }
 
         private static Dictionary<string, string> GetGenerationRate(Entity entity, Dictionary<string, string> stats)
         {
+            ref var generator = ref entity.GetComponent<Generator>(out var exist);
+            if (!exist) return stats;
+            var generationRate = generator.EnergyProductionAmount.Value / generator.BaseCooldown.Value;
+            stats.Add("Generation rate", $"{generationRate:F2}");
             return stats;
         }
 
         private static Dictionary<string, string> GetDamage(Entity entity, Dictionary<string, string> stats)
         {
+            ref var damageDealer = ref entity.GetComponent<DamageDealer>(out var exist);
+            if (!exist) return stats;
+            stats.Add("Damage", damageDealer.Amount.ToString());
             return stats;
         }
 
-        private static Dictionary<string, string> GetAttackRate(Entity entity, Dictionary<string, string> stats)
+        private static Dictionary<string, string> GetShooterStats(Entity entity, Dictionary<string, string> stats)
         {
+            ref var aiming = ref entity.GetComponent<Shooter.Shooter>(out var exist);
+            if (!exist) return stats;
+            var attackRate = 1 / aiming.Cooldown; 
+            stats.Add("Attack rate", $"{attackRate:F2}");
+            stats.Add("Energy cost per shoot", aiming.Cost.ToString());
             return stats;
         }
         
         private static Dictionary<string, string> GetAimingRadius(Entity entity, Dictionary<string, string> stats)
         {
+            ref var aiming = ref entity.GetComponent<Aiming.Aiming>(out var exist);
+            if (!exist) return stats;
+            stats.Add("Aiming radius", $"{aiming.AimingRadius:F1}");
+            return GetAimedState(entity, stats);
+        }
+        
+        private static Dictionary<string, string> GetAimedState(Entity entity, Dictionary<string, string> stats)
+        {
+            entity.GetComponent<Aimed>(out var exist);
+            stats.Add("Aimed", exist ? "True" : "False");
             return stats;
         }
     }
