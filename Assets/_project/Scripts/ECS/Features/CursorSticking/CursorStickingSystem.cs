1ceecf2d02b7b7ded145d0585b1e7b997caacefc
Ninellie <a.pawlow2009@ya.ru>
2025-03-04 17:25:45 +0300

    Added Cursor Sticking system code

--- Assets/_project/Scripts/ECS/Features/CursorSticking/CursorStickingSystem.cs
+++ Assets/_project/Scripts/ECS/Features/CursorSticking/CursorStickingSystem.cs
@@ -1,6 +1,7 @@
 ﻿using Scellecs.Morpeh;
 using Scellecs.Morpeh.Systems;
 using UnityEngine.InputSystem;
+using UnityEngine;
 
 namespace _project.Scripts.ECS.Features.CursorSticking
 {
@@ -8,22 +9,25 @@ namespace _project.Scripts.ECS.Features.CursorSticking
     /// Обрабатывает сущности с компонентом OnCursor
     /// и устанавливает их позицию на позицию курсора
     /// </summary>
-    public class CursorStickingSystem : UpdateSystem
+    [CreateAssetMenu(menuName = "ECS/Systems/Update/" + nameof(CursorStickingSystem))]
+    public sealed class CursorStickingSystem : UpdateSystem
     {
         private Stash<OnCursor> _onCursorStash;
-        
+        private Camera _camera;
+
         public override void OnAwake()
         {
             _onCursorStash = World.GetStash<OnCursor>();
+            _camera = Camera.main;
         }
 
         public override void OnUpdate(float deltaTime)
         {
-            var pointerPos = Pointer.current.position.value; 
-            
+            var pointerPos = Pointer.current.position.value;
+            var pointerPosInWorld = _camera.ScreenToWorldPoint(pointerPos);
             foreach (var onCursor in _onCursorStash)
             {
-                onCursor.Transform.position = pointerPos;
+                onCursor.Transform.position = new Vector3(pointerPosInWorld.x, pointerPosInWorld.y, 0);
             }
         }
     }
--- Assets/_project/Scripts/ECS/Features/CursorSticking/OnCursor.cs
+++ Assets/_project/Scripts/ECS/Features/CursorSticking/OnCursor.cs
@@ -1,11 +1,12 @@
 ﻿using Scellecs.Morpeh;
 using UnityEngine;
-
+using System;
 namespace _project.Scripts.ECS.Features.CursorSticking
 {
     /// <summary>
     /// Сущности с этим компонентом будут прилипать к курсору
     /// </summary>
+    [Serializable]
     public struct OnCursor : IComponent
     {
         public Transform Transform;
